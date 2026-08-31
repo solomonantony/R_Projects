e89c9693c9b0bcec65edb81260e362df16ea3075
Solomon Antony <santony@murraystate.edu>
2026-08-31 16:58:34 -0500

    corrected

--- 01 - Introduction/1DataStructures.r
+++ 01 - Introduction/1DataStructures.r
@@ -3,7 +3,7 @@ patient_names <- c("John Doe", "Jane Doe", 'Joe Crabs', 'Gina Squares')
 temperatures <- c(98.1, 101, 99.5, 101.1)
 
 vaccine_status <- c(TRUE, TRUE, FALSE, TRUE)
-patients[2]
+patient_names[2]
 temperatures[1:3]
 
 temperatures > 100
@@ -14,14 +14,14 @@ gender <- factor(c("Male", "Female", "Male", "Male"))
 gender
 
 patient_names[1]
-patient1 <- list(full_name = patients[1], temperature = temperatures[1], gender=gender[1], vacccine = vaccine_status[1])
+patient1 <- list(full_name = patient_names[1], temperature = temperatures[1], gender=gender[1], vacccine = vaccine_status[1])
 patient1
 patient1[2]
 
-patients_df = data.frame(patients, temperatures, vaccine_status, gender)
+patients_df = data.frame(patient_names, temperatures, vaccine_status, gender)
 patients_df
 
-patients_df$patients
+patients_df$patient_names
 patients_df[1:2] #first and second columns
 patients_df[2,]
 str(patients_df)
--- 01 - Introduction/5histogram_mpg.r
+++ 01 - Introduction/5histogram_mpg.r
@@ -1,3 +1,4 @@
+setwd("../01 - Introduction")
 suv_df <- read.csv("compact_suv_r.csv")
 View(suv_df)
-hist(suv_df$Overall.Miles.Per.Gallon, main = "Null",xlab= "Overall Miles Per Gallon")
+hist(suv_df$Overall.Miles.Per.Gallon,xlab= "Overall Miles Per Gallon", main="Compact cars mileage")
